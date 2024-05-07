99640b7e99e361e0b0abe8a66d740a2b0de5abf4
LJKekana <144112773+thrush01@users.noreply.github.com>
2024-05-07 20:13:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 
+$(document).ready(function() {
+    var table = $('#dataTable').DataTable({
+        "paging": true,
+        "pageLength": 10 // Display maximum 10 rows per page
+    });
+
+    $('#uploadBtn').on('click', function() {
+        var fileInput = document.getElementById('fileInput');
+        var file = fileInput.files[0];
+        if (file) {
+            Papa.parse(file, {
+                header: true,
+                complete: function(results) {
+                    displayData(results.data);
+                }
+            });
+        } else {
+            alert('Please select a file.');
+        }
+    });
+
+    $('#nextBtn').on('click', function() {
+        table.page('next').draw('page');
+    });
+});
+
+function displayData(data) {
+    var table = $('#dataTable').DataTable();
+    table.clear().draw();
+    for (var i = 0; i < data.length; i++) {
+        table.row.add([
+            data[i].Name,
+            data[i].Email
+            // Add more columns as needed
+        ]).draw();
+    }
+}
